5f7a2dd8cdb8b8ad2f06e5cd84dee9f5ea47d2ac
prudentelias1024 <ogundipeoluwabukola2003@gmail.com>
2022-12-23 15:23:15 +0100

    Enrolled Courses Not Enrollable Again
    In Courses,php pages, all courses are showing but enrolled course will not show the enrolled button

--- Dashboard/courses.php
+++ Dashboard/courses.php
@@ -34,87 +34,91 @@
    $courses = $db->getCourses();
    $enrolledCourses = $db->getEnrolledCourse($_SESSION['oracle_no']);
   
-
-  foreach ($courses as $key => $course) {
-    echo '<div class="course border w-fit flex flex-col   px-20 h-fit   py-16 rounded-md ">
-     
+   foreach ($courses as $key => $course) {
+   
+     echo '<div class="course border w-fit flex flex-col   px-20 h-fit   py-16 rounded-md ">
+    
     <div class="flex flex-row mt-6 -ml-12">
 
-       
+      
         <p class="title font-[Mulish] text-4xl font-extrabold ml-2 text-[#939ca5]">'.$course['Training_title'].'</p>
     </div>
 
-    <div class="flex flex-row mt-6 -ml-12 text-2xl">   
-    <p class="code font-[Montserrat] font-extrabold ml-2 text-[#939ca5]">'.$course['Training_Code'].'</p>
-    </div>
-   <div class="flex flex-row gap-48">
+  <div class="flex flex-row mt-6 -ml-12 text-2xl">   
+  <p class="code font-[Montserrat] font-extrabold ml-2 text-[#939ca5]">'.$course['Training_Code'].'</p>
+  </div>
+ <div class="flex flex-row gap-48">
 
-    <div class="flex flex-col mt-6 -ml-12">
+  <div class="flex flex-col mt-6 -ml-12">
 
-     <p class="font-[Mulish] font-semibold text-[#b8b8b8]">Participants</p> <br> 
+   <p class="font-[Mulish] font-semibold text-[#b8b8b8]">Participants</p> <br> 
 
-     <div class="participants flex flex-row -mt-8">';
-     $images =  $db->getTrainingParticipants($course['Training_Code']);
-     foreach ($images as $image) {
-  
-     echo  '<img src=".'.$image["Image"].'" alt="" class="rounded-full  w-10  h-10 object-cover ">
-     
-        </div>
-    </div>';    
+   <div class="participants flex flex-row -mt-8">';
+   $images =  $db->getTrainingParticipants($course['Training_Code']);
+   foreach ($images as $image) {
 
-     }
-     if (in_array($course["Training_Code"],$enrolledCourses)) {
-    
-    echo '<div class="container flex-col relative left-16 top-4 ">
-
-  <p class="font-[Mulish] font-semibold mb-2 text-[#b8b8b8] ">Progress</p>
-    <div class="row">
-        <div class="col-md-3 col-sm-6">
-            <div class="prog blue">
-                <span class="prog-left">
-                  <span class="prog-bar"></span>
-                </span>
-                <span class="prog-right">
-                    <span class="prog-bar"></span>
-                </span>
-                <div class="prog-value font-bold">90%</div>
-            </div>
-        </div>
-</div>
-
-</div>
+   echo  '<img src=".'.$image["Image"].'" alt="" class="rounded-full  w-10  h-10 object-cover ">
+   
       </div>
-      
+  </div>
+  ';    
+
+   }
+  echo '<div class="container flex-col relative left-16 top-4 ">
+
+<p class="font-[Mulish] font-semibold mb-2 text-[#b8b8b8] ">Progress</p>
+  <div class="row">
+      <div class="col-md-3 col-sm-6">
+          <div class="prog blue">
+              <span class="prog-left">
+                <span class="prog-bar"></span>
+              </span>
+              <span class="prog-right">
+                  <span class="prog-bar"></span>
+              </span>
+              <div class="prog-value font-bold">90%</div>
+          </div>
+      </div>
+</div>
+</div>
+</div>
+   
+    
 <div class="flex flex-row mt-12 justify-between -ml-20 -mb-12">
-  <div class="flex flex-row gap-2 ml-6">
-    <i class="fa-solid fa-user text-[#b8b8b8] mt-1"></i>
-  <p class="font-[Mulish] text-[#adadad] font-bold">'.$course['Course_Coordinator'].'</p>
+<div class="flex flex-row gap-2 ml-6">
+  <i class="fa-solid fa-user text-[#b8b8b8] mt-1"></i>
+<p class="font-[Mulish] text-[#adadad] font-bold">'.$course['Course_Coordinator'].'</p>
 </div>
 <div class="flex flex-row relative left-16 gap-1">
-  <i class="fa-solid fa-list text-[#adadad] mt-1 "></i>
-  <p class="font-[Mulish] text-[#adadad] font-bold">'.$course['No_Of_Classes'].' Classes</p>
+<i class="fa-solid fa-list text-[#adadad] mt-1 "></i>
+<p class="font-[Mulish] text-[#adadad] font-bold">'.$course['No_Of_Classes'].' Classes</p>
 
-  </div>
 </div>
-
-<form action="./enrol.php" method="post" class="w-full">
-  
+</div>';
+foreach ($enrolledCourses as  $enrolledCourse) {
+ 
+  if (in_array($course["Training_Code"],$enrolledCourse,true)) {
+    echo  '<form action="./enrol.php" method="post" class="w-full">
+    
     <button type="submit" id="enrol"style="width: inherit;" onclick="removeCourse(event)" class=" bg-[#512bd4] mt-14 text-white py-3 rounded-md -mb-12">Enrol</button>
     <input type="hidden" name="t_code" value="'.$course['Training_Code'].'"/>
     <input type="hidden" name="t_coordinator" value="'.$course['Course_Coordinator'].'"/>
     <input type="hidden" name="t_title" value="'.$course['Training_title'].'"/>
-    </form>
-    </div>';
+    </form>';
+  }
 }
+echo '
+</div>
+';
 
-  
 }
+  
 ?>
 
 
-
+</div>
 </div>
 
-    </div>
+
   </body>
 </html>
